774fb6bb4e607c82e15187ee5e0a4f329445a6b4
mura012 <murakoudai@gmail.com>
2022-12-12 11:24:58 +0900

    mantineをインストール

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@mantine/core";
+
 export default function Home() {
   return (
     <>
+      <Button />
       <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
         Hello world!
       </h1>
