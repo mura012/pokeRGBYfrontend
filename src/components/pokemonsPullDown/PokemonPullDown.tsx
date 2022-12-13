1bc43224273eca8246569dcff7bcb376bec33919
mura012 <murakoudai@gmail.com>
2022-12-13 17:34:44 +0900

    プルダウンの選択肢に番号やタイプも記載

--- src/components/pokemonsPullDown/PokemonPullDown.tsx
+++ src/components/pokemonsPullDown/PokemonPullDown.tsx
@@ -10,12 +10,24 @@ type Props = {
 export const PokemonPullDown = ({ selected, setSelected }: Props) => {
   return (
     <>
-      <select value={selected} onChange={(e) => setSelected(e.target.value)}>
+      <select
+        value={selected}
+        onChange={(e) => setSelected(e.target.value)}
+        className="p-3"
+      >
         <option value="---">---</option>
-        {PokemonData.map((pokemon: Pokemon) => {
+        {PokemonData.map(({ name, number, typeA, typeB, point }: Pokemon) => {
           return (
-            <option value={pokemon.name} key={pokemon.name}>
-              {pokemon.name}
+            <option value={name} key={number}>
+              <p>
+                {number < 10 ? "0" : ""}
+                {number}
+              </p>{" "}
+              <p>{name}</p>
+              {"　"}
+              {name.length === 4 ? "　" : ""}
+              {name.length === 3 ? "　　" : ""}
+              <p>{typeA}</p> <p>{typeB}</p>
             </option>
           );
         })}
