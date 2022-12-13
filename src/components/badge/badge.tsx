0607ec152368479b6a681ee89605501ab4f8291b
mura012 <murakoudai@gmail.com>
2022-12-13 11:35:49 +0900

    タイプによって色がついてなかったので[bg-]も渡して解決

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -9,7 +9,7 @@ export const Badge = ({ children, color }: Props) => {
   console.log(color);
 
   return (
-    <div className={`w-fit rounded-xl bg-${color} px-2 py-1 text-xs font-bold`}>
+    <div className={`w-fit rounded-xl ${color} px-2 py-1 text-xs font-bold`}>
       {children}
     </div>
   );
