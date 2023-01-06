47cc64149e36b81c1b75e3fa28c5aa3908aa0e0a
mura012 <murakoudai@gmail.com>
2023-01-06 22:45:09 +0900

    hamburgerメニューを実装

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,36 @@
+import { Drawer, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { links } from "libs/links";
 import Link from "next/link";
 
 export const Header = () => {
+  const [opened, { toggle, close }] = useDisclosure(false);
+
+  const link = links.map((link) => {
+    return (
+      <li key={link.id}>
+        <Link href={link.href}>{link.label}</Link>
+      </li>
+    );
+  });
   return (
-    <header className="flex items-center justify-around border-0 border-b border-solid font-bold">
-      <p>aiu</p>
+    <header className="flex h-12 items-center justify-around border-0 border-b border-solid font-bold">
+      <Drawer
+        opened={opened}
+        onClose={() => toggle()}
+        title="PokeRGBY"
+        padding="xl"
+        size="xl"
+      >
+        <ul className="space-y-2 text-xl">{link}</ul>
+      </Drawer>
+      <Burger opened={opened} onClick={toggle} className="xs:hidden">
+        Open Drawer
+      </Burger>
+
+      <p>pokeRGBY</p>
       <nav>
-        <ul className="flex space-x-12">
-          {links.map((link) => {
-            return (
-              <li key={link.id}>
-                <Link href={link.href}>{link.label}</Link>
-              </li>
-            );
-          })}
-        </ul>
+        <ul className="hidden space-x-12 xs:flex">{link}</ul>
       </nav>
     </header>
   );
