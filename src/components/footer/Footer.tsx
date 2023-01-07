49acb96eee4dd3bb16ef1fd41034971090408f97
mura012 <murakoudai@gmail.com>
2023-01-07 19:44:56 +0900

    footerの名前の部分をTwitterのリンクにした

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
+
 export const Footer = () => {
   const date = new Date();
   return (
     <footer className="flex h-12 justify-center border-0 border-t border-solid text-center">
-      <p>© {date.getFullYear()} 武良</p>
+      <div className="flex items-center">
+        <p>© {date.getFullYear()}</p>
+        <Link
+          href="https://twitter.com/most_love08"
+          className="text-blue-400 hover:text-red-600"
+        >
+          武良
+        </Link>
+      </div>
     </footer>
   );
 };
