52b45d9b4c5131b21afa93d695d3a4a782009dfa
github user mura012 <murakoudai@gmail.com>
2023-01-08 21:57:45 +0900

    envファイルから取得

--- src/fooks/useGetPokemon.ts
+++ src/fooks/useGetPokemon.ts
@@ -3,7 +3,6 @@ import { PokemonType } from "types/pokemon";
 export const useGetPokemon = () => {
   const fetcher = (url: string) => fetch(url).then((r) => r.json());
   const { data, error }: { data: PokemonType; error: Error | undefined } =
-    useSWR("/api/pokemon", fetcher);
-
+    useSWR(`${process.env.NEXT_PUBLIC_API_URL}`, fetcher);
   return { data, error };
 };
