c8b3179ef45b1099a562fb53226470ff8a4b9098
github user mura012 <murakoudai@gmail.com>
2023-01-09 08:19:49 +0900

    apiの叩き方を変更

--- src/fooks/useGetPokemon.ts
+++ src/fooks/useGetPokemon.ts
@@ -3,6 +3,7 @@ import { PokemonType } from "types/pokemon";
 export const useGetPokemon = () => {
   const fetcher = (url: string) => fetch(url).then((r) => r.json());
   const { data, error }: { data: PokemonType; error: Error | undefined } =
-    useSWR(`${process.env.NEXT_PUBLIC_API_URL}`, fetcher);
+    useSWR("api/pokemon", fetcher);
+
   return { data, error };
 };
