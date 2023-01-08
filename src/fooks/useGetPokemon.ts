6c0eb8117db0d61d1073429064c51c9c2c919a7c
github user mura012 <murakoudai@gmail.com>
2023-01-08 22:10:16 +0900

    変更

--- src/fooks/useGetPokemon.ts
+++ src/fooks/useGetPokemon.ts
@@ -3,6 +3,6 @@ import { PokemonType } from "types/pokemon";
 export const useGetPokemon = () => {
   const fetcher = (url: string) => fetch(url).then((r) => r.json());
   const { data, error }: { data: PokemonType; error: Error | undefined } =
-    useSWR(`${process.env.NEXT_PUBLIC_API_URL}`, fetcher);
+    useSWR("https://pokemonrgbytoolsbackend.onrender.com/api/pokemon", fetcher);
   return { data, error };
 };
