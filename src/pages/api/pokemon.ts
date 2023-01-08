d0bce22aa440dc0ee862a7940d2e0ecd8e71ff54
github user mura012 <murakoudai@gmail.com>
2023-01-08 21:21:35 +0900

    urlをenvファイルで定義

--- src/pages/api/pokemon.ts
+++ src/pages/api/pokemon.ts
@@ -5,7 +5,7 @@ const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<PokemonType>
 ) => {
-  const data = await fetch("http://localhost:8080/api/pokemon");
+  const data = await fetch(`${process.env.API_URL}api/pokemon`);
   const json = await data.json();
   res.status(200).json(json);
 };
