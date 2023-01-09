b0d7364dc7afe472a6251017ceb14f3f12c0df58
mura012 <murakoudai@gmail.com>
2023-01-09 15:47:48 +0900

    エラーを解決

--- src/fooks/useGetPokemon.ts
+++ src/fooks/useGetPokemon.ts
@@ -4,10 +4,7 @@ export const useGetPokemon = () => {
   const fetcher = (url: string) => fetch(url).then((r) => r.json());
   const { data, error }: { data: PokemonType; error: Error | undefined } =
     // useSWR("api/pokemon", fetcher);
-    useSWR(
-      "https://pokemonrgbytoolsbackend.onrender.com/api/pokemon/ほのお",
-      fetcher
-    );
+    useSWR("https://pokemonrgbytoolsbackend.onrender.com/api/pokemon", fetcher);
 
   return { data, error };
 };
