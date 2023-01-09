5cc1fc58329817e0581a6afa4a5a783950cc4142
mura012 <murakoudai@gmail.com>
2023-01-09 15:21:38 +0900

    apiを直接たたく

--- src/fooks/useGetPokemonSearch.ts
+++ src/fooks/useGetPokemonSearch.ts
@@ -1,11 +1,12 @@
 import useSWR from "swr";
 import { PokemonType } from "types/pokemon";
-export const useGetPokemon = () => {
+export const useGetPokemonSearch = (type: string) => {
+  console.log("in");
+
   const fetcher = (url: string) => fetch(url).then((r) => r.json());
   const { data, error }: { data: PokemonType; error: Error | undefined } =
-    // useSWR("api/pokemon", fetcher);
-    // useSWR("http://localhost:8080/api/pokemon", fetcher);
-    useSWR("https://pokemonrgbytoolsbackend.onrender.com/api/pokemon", fetcher);
+    useSWR(`http://localhost:8080/api/pokemon/${type}`, fetcher);
+  // useSWR(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/${type}`, fetcher);
 
   return { data, error };
 };
