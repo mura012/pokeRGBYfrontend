e9081f5b78c293f15105612c2423e68d2eabf474
mura012 <murakoudai@gmail.com>
2023-01-10 17:08:26 +0900

    hamburgerメニューとfooterのスタイルを変更

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,16 +3,14 @@ import Link from "next/link";
 export const Footer = () => {
   const date = new Date();
   return (
-    <footer className="flex h-12 justify-center border-0 border-t border-solid text-center">
-      <div className="flex items-center">
-        <p>© {date.getFullYear()}</p>
-        <Link
-          href="https://twitter.com/most_love08"
-          className="text-blue-400 hover:text-red-600"
-        >
-          武良
-        </Link>
-      </div>
+    <footer className="flex h-12 items-center justify-center bg-gray-500 text-center text-gray-100">
+      <p>© {date.getFullYear()}</p>
+      <Link
+        href="https://twitter.com/most_love08"
+        className="text-blue-400 hover:text-red-600"
+      >
+        武良
+      </Link>
     </footer>
   );
 };
